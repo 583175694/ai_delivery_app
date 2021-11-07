927d4d9d6d3e84eb084e4e792517eb3eaf3bfc86
pickle_tiger <1062155964@qq.com>
2021-11-08 00:06:38 +0800

    完成投递详情卡片静态样式

---
+++ lib/components/my_underline_tabIndicator.dart
@@ -0,0 +1,91 @@
+/**
+ * @ClassName 参考TabIndicator 增加宽度及圆角
+ * @Author gaowutai
+ * @Date 2021-11-6
+ **/
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+class MyUnderlineTabIndicator extends Decoration {
+  const MyUnderlineTabIndicator({
+    this.borderSide = const BorderSide(width: 2.0, color: Colors.white),
+    this.insets = EdgeInsets.zero,
+  }) : assert(borderSide != null),
+        assert(insets != null);
+
+  /// The color and weight of the horizontal line drawn below the selected tab.
+  final BorderSide borderSide;
+
+  /// Locates the selected tab's underline relative to the tab's boundary.
+  ///
+  /// The [TabBar.indicatorSize] property can be used to define the tab
+  /// indicator's bounds in terms of its (centered) tab widget with
+  /// [TabBarIndicatorSize.label], or the entire tab with
+  /// [TabBarIndicatorSize.tab].
+  final EdgeInsetsGeometry insets;
+
+  @override
+  Decoration? lerpFrom(Decoration? a, double t) {
+    if (a is MyUnderlineTabIndicator) {
+      return MyUnderlineTabIndicator(
+        borderSide: BorderSide.lerp(a.borderSide, borderSide, t),
+        insets: EdgeInsetsGeometry.lerp(a.insets, insets, t)!,
+      );
+    }
+    return super.lerpFrom(a, t);
+  }
+
+  @override
+  Decoration? lerpTo(Decoration? b, double t) {
+    if (b is MyUnderlineTabIndicator) {
+      return MyUnderlineTabIndicator(
+        borderSide: BorderSide.lerp(borderSide, b.borderSide, t),
+        insets: EdgeInsetsGeometry.lerp(insets, b.insets, t)!,
+      );
+    }
+    return super.lerpTo(b, t);
+  }
+
+  @override
+  BoxPainter createBoxPainter([ VoidCallback? onChanged ]) {
+    return _UnderlinePainter(this, onChanged);
+  }
+
+  Rect _indicatorRectFor(Rect rect, TextDirection textDirection) {
+    assert(rect != null);
+    assert(textDirection != null);
+    final Rect indicator = insets.resolve(textDirection).deflateRect(rect);
+
+    double wantWidth = 32;
+    //取中间坐标
+    double cw = (indicator.left + indicator.right) / 2;
+
+    return Rect.fromLTWH(cw - wantWidth / 2,
+        indicator.bottom - borderSide.width, wantWidth, borderSide.width);
+  }
+
+  @override
+  Path getClipPath(Rect rect, TextDirection textDirection) {
+    return Path()..addRect(_indicatorRectFor(rect, textDirection));
+  }
+}
+
+class _UnderlinePainter extends BoxPainter {
+  _UnderlinePainter(this.decoration, VoidCallback? onChanged)
+      : assert(decoration != null),
+        super(onChanged);
+
+  final MyUnderlineTabIndicator decoration;
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    assert(configuration != null);
+    assert(configuration.size != null);
+    final Rect rect = offset & configuration.size!;
+    final TextDirection textDirection = configuration.textDirection!;
+    final Rect indicator = decoration._indicatorRectFor(rect, textDirection).deflate(decoration.borderSide.width / 2.0);
+    final Paint paint = decoration.borderSide.toPaint()..strokeCap = StrokeCap.round;
+    canvas.drawLine(indicator.bottomLeft, indicator.bottomRight, paint);
+  }
+}
