e242c905cc7a7e23dbe54fb6021ab03e33689cd0
wushaohang <583175694@qq.com>
2021-11-08 15:38:59 +0800

    base

---
+++ lib/plugins/base.dart
@@ -0,0 +1,19 @@
+library base;
+/*
+ * @discripe: 全局公共类管理
+ */
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:AI_delivery_app/plugins/screen_fit.dart';
+
+
+// 所有Widget继承的抽象类
+abstract class Base {
+  // 初始化设计稿尺寸
+  static final double designWidth = 750;
+  static final double designHeight = 1624;
+
+  // flutter_ScreenUtil px转dp
+  num dp(double designValue) => ScreenUtil.getInstance().setWidth(designValue);
+}
